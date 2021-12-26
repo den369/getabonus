f26f4c13250bebdcb602489d5d3d84be0deca841
Denis Petrovskii <denpetrovsky369@gmail.com>
2021-12-26 13:08:51 +0300

    triitogo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        int startBalance = 100;
-        int added = 1100;
+        int startBalance = 150;
+        int added = 1350;
         int bonus = added / 100;
 
         int finishBalance;
-        if (added >= 1100) {
+        if (added >= 1000) {
             finishBalance = startBalance + added + bonus;
         } else {
             finishBalance = startBalance + added;
         }
 
+        System.out.println("Вы пополнили счет на " + added + " руб.");
+        System.out.println("Ваш бонус составил " + bonus + " руб.");
+        System.out.println("И теперь ваш баланс " + finishBalance + " руб.");
 
-        System.out.println(finishBalance);
     }
 }
 
